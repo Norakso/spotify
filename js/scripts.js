eabe292a36ee2f72a9f9d9c42aac33a5b8a0bacc
Nora Karaduman Sorsenger <noraks@Noras-MacBook-Pro.local>
2023-02-12 15:25:06 +0100

    fixed first page

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,11 @@ var myTimer;
 
 function page(n) {
     clearInterval(myTimer);
-    myTimer = setInterval(function() {plusPages(n + 1)}, 10000);
+    if (pageIndex + n <= 1) {
+        myTimer = setInterval(function(){plusPages(n + 1)}, 10000);
+    } else {
+        myTimer = setInterval(function(){plusPages(n + 1)}, 15000);
+    }
     showPage(pageIndex += n);
 }
 
@@ -21,6 +25,8 @@ function showPage(n) {
         page[0].id = pages[n-1];
     }
 
+    newPage(n);
+
     if (pageIndex > pages.length-1) {
         pageIndex = pages.length
     }
@@ -30,6 +36,8 @@ function showPage(n) {
 
     var progress = document.getElementsByClassName("progression");
 
+    var toggle = 0;
+
     for (i = 0; i < progress.length; i++) {
         if (i < n-1) {
             progress[i].style.width = "100%";
@@ -38,10 +46,18 @@ function showPage(n) {
         }
         if (i == n-1) {
             progress[i].id = "animateProgress";
-        } else {
-            if (progress[i].getAttribute('id') != null) {
-                progress[i].removeAttribute('id');
+        } else if (n == 0) {
+            if (progress[n].id == "animateProgress" && toggle == 0) {
+                progress[n].removeAttribute('id');
+                progress[n].id = "reanimateProgress";
+                toggle = 1;
+            } else if (progress[n].id == "reanimateProgress" && toggle == 0) {
+                progress[n].removeAttribute('id');
+                progress[n].id = "animateProgress";
+                toggle = 1;
             }
+        } else if (progress[i].getAttribute('id') != null) {
+            progress[i].removeAttribute('id');
         }
     }
 }
@@ -53,11 +69,40 @@ function plusPages(n){
     } else {
     showPage(pageIndex += 1); 
     }
-    
-    myTimer = setInterval(function(){plusPages(n + 1)}, 10000);
+    if ((n == 0 || n == -1) && document.getElementsByClassName("screen").id == "page1") {
+        myTimer = setInterval(function(){plusPages(n + 1)}, 10000);
+    } else {
+        myTimer = setInterval(function(){plusPages(n + 1)}, 15000);
+    }
 }
 
 window.addEventListener("load",function() {
     showPage(pageIndex);
     myTimer = setInterval(function(){plusPages(1)}, 10000);
 });
+
+function newPage(n) {
+    document.getElementById("content").innerHTML = "";
+    if (n == 1 || n == 0) {
+        var heading = document.createElement("h1");
+        heading.classList.add("heading");
+        heading.innerHTML = "Nora Wrapped";
+
+        var creator = document.createElement("p");
+        creator.classList.add("creator");
+        creator.innerHTML = "by Nora Sorsenger";
+
+        var hashtag = document.createElement("p");
+        hashtag.classList.add("hashtag");
+        hashtag.innerHTML = "#NORAWRAPPED";
+
+        var smallCircle = document.createElement("div");
+        smallCircle.classList.add("circle", "smallCircle");
+        var smallCircle2 = document.createElement("div");
+        smallCircle2.classList.add("circle", "smallCircle2");
+        var bigCircle = document.createElement("div");
+        bigCircle.classList.add("circle", "bigCircle");
+
+        document.getElementById("content").append(smallCircle, smallCircle2, bigCircle, heading, creator, hashtag);
+    }
+}
